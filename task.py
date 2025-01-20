21fb206a26e4dbe647983be64189e2829ebbdbf4
prakriti khatri <prakritikhatri746@gmail.com>
2025-01-20 16:50:31 +0545

    any message

--- task.py
+++ task.py
@@ -59,3 +59,79 @@ print("array2")
 print(array2)
 print("\nmatrix multiplication result")
 print(result)
+
+#cvs
+import csv
+data_path = "employees.csv"
+
+def create_csv():
+    headers = ["Name", "Age", "Department",]
+    with open(data_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+    print("File has been created.")
+
+def add_data(Name, Age, Department):
+    with open(data_path, mode = "a", newline = "") as file:
+        writer = csv.writer(file)
+        writer.writerow([Name, Age, Department])
+
+def read_csv():
+    with open(data_path, mode="r") as file:
+        reader = csv.DictReader(file)
+        print("\nEmployees older than 30:")
+        for row in reader:
+            name = row["Name"]
+            age = int(row["Age"])
+            if age > 30:
+                print(name)
+create_csv()
+add_data("Roshna", 19, "Finance")
+add_data("Salina", 35, "Marketing")
+add_data("Prakriti", 39, "IT department")
+add_data("Ashmita", 60, "Production")
+add_data("Roar", 29, "HR")
+
+read_csv()
+
+import openpyxl
+student_file_path = "results.xlsx"
+
+def create_messy_dataset(student_file_path):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Students Grades"
+
+    sheet.append(["Student Name", "Grade"])
+    sheet.append(["Roshna", "A"]) 
+    sheet.append(["Salina", "B+"])
+    sheet.append(["Prakriti", "A-"])
+    sheet.append(["Ashmita", "B"])
+    sheet.append(["pratushya", "A+"])
+
+    workbook.save(student_file_path)
+    print(f"Dataset created and saved to '{student_file_path}'.")
+
+create_messy_dataset(student_file_path)
+
+#http
+import requests
+api_url = "https://jsonplaceholder.typicode.com"
+def fetch_and_display_titles(api_url):
+    try:
+        # Making a GET request to the API
+        response = requests.get(api_url)
+        
+        # Checking if the request was successful
+        if response.status_code == 200:
+            posts = response.json()  # Parse the JSON data
+            
+            print("Titles of the first 5 posts:")
+            for i, post in enumerate(posts[:5], start=1):  # Display titles of first 5 posts
+                print(f"{i}. {post['title']}")
+        else:
+            print(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
